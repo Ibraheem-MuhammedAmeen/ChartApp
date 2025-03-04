5f578a137d731fee5b060d0b52b8bd0a1a366223
Ibraheem Ameen <ibraheemameen941@gmail.com>
2025-03-04 22:56:54 +0100

    Updated messages page UI

--- lib/pages/messages_page.dart
+++ lib/pages/messages_page.dart
@@ -1,7 +1,10 @@
+import 'package:chatter/models/story_data.dart';
 import 'package:chatter/widgets/icon_buttons.dart';
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 
 import '../helpers.dart';
+import '../models/message_data.dart';
 import '../theme.dart';
 import '../widgets/avatar.dart';
 
@@ -36,16 +39,171 @@ class MessagesPage extends StatelessWidget {
           ),
         ],
       ),
-      body: Card(
-        color: Color(0xFF7F7FF),
-        margin: const EdgeInsets.all(8),
-        elevation: 0,
-        child: SizedBox(
-          height: 134,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Stories(),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Text('');
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+    Widget _delegate(BuildContext context, int index) {
+      final Faker faker = Faker();
+      final date = Helpers.randomDate();
+      return _MessageTitle(
+        messageData: MessageData(
+          senderName: faker.person.name(),
+          message: faker.lorem.sentence(),
+          messageDate: date,
+          dateMessage: Jiffy(date).fromNow(),
+          profilePicture: Helpers.randomPictureUrl(),
+        ),
+      );
+    }
+  }
+}
+
+class _MessageTitle extends StatelessWidget {
+  const _MessageTitle({
+    Key? key,
+    required this.messageData,
+  }) : super(key: key);
+
+  final MessageData messageData;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        //Navigator.of(context).push(ChatScreen.route(messageData));
+      },
+      child: Container(
+        height: 100,
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        decoration: const BoxDecoration(
+          border: Border(
+            bottom: BorderSide(
+              color: Colors.grey,
+              width: 0.2,
+            ),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Row(
             children: [
-              Text(
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Avatar.medium(url: messageData.profilePicture),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text(
+                        messageData.senderName,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          letterSpacing: 0.2,
+                          wordSpacing: 1.5,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                      child: Text(
+                        messageData.message,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          fontSize: 12,
+                          color: AppColors.textFaded,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      messageData.dateMessage.toUpperCase(),
+                      style: const TextStyle(
+                        fontSize: 11,
+                        letterSpacing: -0.2,
+                        fontWeight: FontWeight.w600,
+                        color: AppColors.textFaded,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Container(
+                      width: 18,
+                      height: 18,
+                      decoration: const BoxDecoration(
+                        color: AppColors.secondary,
+                        shape: BoxShape.circle,
+                      ),
+                      child: const Center(
+                        child: Text(
+                          '1',
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: AppColors.textLigth,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Stories extends StatelessWidget {
+  const Stories({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white10,
+      //color: Color(0xFF7F7FF),
+      elevation: 0,
+      child: SizedBox(
+        height: 134,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 8,
+                bottom: 16,
+                left: 16.0,
+              ),
+              child: Text(
                 'Stories',
                 style: TextStyle(
                   fontWeight: FontWeight.w900,
@@ -53,17 +211,57 @@ class MessagesPage extends StatelessWidget {
                   color: AppColors.textFaded,
                 ),
               ),
-              Expanded(
-                  child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (BuildContext context, int index) {
-                  return Text('texing');
-                },
-              ))
-            ],
-          ),
+            ),
+            Expanded(
+                child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    width: 60,
+                    child: StoryCard(
+                      storyData: StoryData(
+                        name: faker.person.name(),
+                        url: Helpers.randomPictureUrl(),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ))
+          ],
         ),
       ),
+    );
+  }
+}
+
+class StoryCard extends StatelessWidget {
+  const StoryCard({
+    super.key,
+    required this.storyData,
+  });
+
+  final StoryData storyData;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Avatar.medium(url: storyData.url),
+        Expanded(
+            child: Padding(
+          padding: const EdgeInsets.only(top: 16),
+          child: Text(
+            storyData.name,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(
+                fontSize: 11, letterSpacing: 0.3, fontWeight: FontWeight.bold),
+          ),
+        ))
+      ],
     );
   }
 }
